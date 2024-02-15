1dd6bb733543245707c0c33248bd59278e88de34
AJ Otto <ajottoman@gmail.com>
2024-02-15 11:18:41 +0200

    [IMP] Initial migration improved

--- migrations/migration-01.sql
+++ migrations/migration-01.sql
@@ -1,10 +1,27 @@
 -- Shopping list seeding DDL script
 -- Created: 2024-02-15
 
-CREATE TABLE shopping_list (
+-- create db if not exists
+CREATE DATABASE IF NOT EXISTS ShoppingListDB;
+
+-- use db
+USE ShoppingListDB;
+
+-- create table if not exists
+CREATE TABLE IF NOT EXISTS shopping_list (
     id INT AUTO_INCREMENT PRIMARY KEY,
     product_title VARCHAR(50) NOT NULL,
     is_checked BOOLEAN DEFAULT FALSE,
-    -- column for soft delete
     is_deleted BOOLEAN DEFAULT FALSE
 );
+
+-- insert default data if not exists
+INSERT INTO shopping_list (product_title)
+SELECT 'Milk' 
+WHERE NOT EXISTS (SELECT 1 FROM shopping_list WHERE product_title = 'Milk')
+UNION ALL
+SELECT 'Bread'
+WHERE NOT EXISTS (SELECT 1 FROM shopping_list WHERE product_title = 'Bread')
+UNION ALL
+SELECT 'Eggs'
+WHERE NOT EXISTS (SELECT 1 FROM shopping_list WHERE product_title = 'Eggs');
